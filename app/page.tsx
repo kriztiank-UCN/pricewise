9341233e1d0e0fe323b0e736359871b088136474
Kristian <10221425@ucn.dk>
2024-08-22 20:55:56 +0200

    Product Details Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 import HeroCarousel from "@/components/HeroCarousel"
 import Searchbar from "@/components/Searchbar"
 import Image from "next/image"
+// import server actions
+import { getAllProducts } from "@/lib/actions"
+import ProductCard from "@/components/ProductCard"
 
-const Home = () => {
+const Home = async () => {
+  const allProducts = await getAllProducts()
+
   return (
     <>
       <section className="px-6 md:px-20 py-24">
@@ -20,10 +25,10 @@ const Home = () => {
               Powerful, self-serve product and growth analytics to help you convert, engage, and
               retain more.
             </p>
-            
+
             <Searchbar />
           </div>
-          
+
           <HeroCarousel />
         </div>
       </section>
@@ -32,11 +37,8 @@ const Home = () => {
         <h2 className="section-text">Trending</h2>
 
         <div className="flex flex-wrap gap-x-8 gap-y-16">
-          {/* {allProducts?.map((product) => (
+          {allProducts?.map(product => (
             <ProductCard key={product._id} product={product} />
-          ))} */}
-          {["Iphone", "Samsung", "Nokia", "Motorola"].map(product => (
-            <div>{product}</div>
           ))}
         </div>
       </section>
